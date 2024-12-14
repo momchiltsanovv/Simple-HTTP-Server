537a07459c3801784179ce711d6c1c3878775326
Momchil Tsanov <momchil395@gmail.com>
2024-12-14 19:17:11 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening on port 8080 ...");
+
+        while (true) {
+
+        }
+    }
+}
