b4b28d5b51d8120eba3551994131e4fc92d3a06f
Momchil Tsanov <momchil395@gmail.com>
2024-12-14 20:26:25 +0200

    importing classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
@@ -12,15 +14,15 @@ public class Main {
         System.out.println("Listening on port 8080 ...");
 
         while (true) {
-            //            final Socket client = server.accept();
-            //            InputStreamReader isr
-            //                    = new InputStreamReader(client.getInputStream());
-            //            BufferedReader reader = new BufferedReader(isr);
-            //            String line = reader.readLine();
-            //            while (!line.isEmpty()) {
-            //                System.out.println(line);
-            //                line = reader.readLine();
-            //            }
+//                        final Socket client = server.accept();
+//                        InputStreamReader isr
+//                                = new InputStreamReader(client.getInputStream());
+//                        BufferedReader reader = new BufferedReader(isr);
+//                        String line = reader.readLine();
+//                        while (!line.isEmpty()) {
+//                            System.out.println(line);
+//                            line = reader.readLine();
+//                        }
             try (Socket socket = server.accept()) { // socket send and receive from clients
                 Date today = new Date();
                 String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
